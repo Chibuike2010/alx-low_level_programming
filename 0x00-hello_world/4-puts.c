5862a6de8207ce0afba5006e52259aa8be600f37
chibuike2010 <chibuike.sunday67@gmail>
2023-02-17 09:13:40 -0800

    5th commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ *main - this is the main function
+ *
+ *Return: return 0
+ */
+
 int main(void)
 
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
