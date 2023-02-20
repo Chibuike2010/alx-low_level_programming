fcefcc559fee9c9ca3cb40cdf91a92c35f272915
chibuike2010 <chibuike.sunday67@gmail>
2023-02-20 11:09:08 -0800

    4th commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - this is main
+ *
+ *Return: returns 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z'; i++)
+		if (i != 'e' && i != 'p')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
