7c2edb51f1936a12c331742cb41b27ce9664f9f1
chibuike2010 <chibuike.sunday67@gmail>
2023-02-20 09:05:50 -0800

    2nd commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *main - prints loop in lower case
+ *
+ *Return: returns 0
+ */
+int main(void)
+{
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
