2590bb8e632adb8363e8cd3db513ec611233dcc6
chibuike2010 <chibuike.sunday67@gmail>
2023-02-20 10:05:27 -0800

    3rd commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - putchar lower and upper case
+ *
+ *Return: returns 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
